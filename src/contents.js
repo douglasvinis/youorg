8f6283439e6e04ee7841a2863a626bffdaef11cf
Douglas Vinicius <douglvini@gmail.com>
2020-11-24 16:19:43 -0300

    - All functionality intended from groups config section is working fully.

--- src/contents.js
+++ src/contents.js
@@ -6,6 +6,8 @@
 
 'use strict';
 
+const FEED_BASE_URL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+
 let videos = document.getElementById("videos");
 let video_entry_temp = document.getElementById("video_entry_temp").content;
 
@@ -17,7 +19,7 @@ function show_channel_group(channel_id_list)
 	{
 		let request = new XMLHttpRequest();
 		request.open("GET",
-				"https://www.youtube.com/feeds/videos.xml?channel_id=" + channel_id_list[channel_i],
+				FEED_BASE_URL + channel_id_list[channel_i],
 				false);
 		request.send(null);
 		let channel_videos = (new DOMParser()).parseFromString(request.responseText, "text/xml");
@@ -84,6 +86,7 @@ function show_channel_group(channel_id_list)
 let navbar = document.getElementById("navbar");
 let group_entry_temp = document.getElementById("group_entry_temp").content;
 let group_config_temp = document.getElementById("group_config_temp").content;
+let config_entry_temp = document.getElementById("config_entry_temp").content;
 let chosen_group_entry = null;
 
 browser.storage.local.get({group_list: []}).then(data =>
@@ -113,50 +116,125 @@ browser.storage.local.get({group_list: []}).then(data =>
 		chosen_group_entry.setAttribute("id", "chosen");
 
 		let group_config = group_config_temp.querySelector("div").cloneNode(true);
-		group_config.querySelector("#delete").innerText = "Delete Group";
-
-		let title_input = group_config.querySelector("#title");
-		let apply_button = group_config.querySelector("#apply");
-		title_input.value = name;
-		apply_button.innerText = "Apply Changes";
-		apply_button.addEventListener("click", evt =>
+		let delete_button = group_config.querySelector("#delete");
+		delete_button.innerText = "Delete Group";
+		delete_button.addEventListener("click", evt =>
 		{
-			let new_group_name = title_input.value;
-			if ((new_group_name != name) && (new_group_name != ""))
+			if (confirm("Delete the group and all channels added ?"))
 			{
-				let group_data_tmp = {};
-				group_data_tmp[name] = [];
-				browser.storage.local.get(group_data_tmp).then(group_data =>
-				{
-					group_data_tmp = {};
-					group_data_tmp[new_group_name] = group_data[name];
-					browser.storage.local.set(group_data_tmp).then(() =>
-					{
-						browser.storage.local.remove(name);
-					});
-				});
 				browser.storage.local.get({group_list: []}).then(group_list_data =>
 				{
 					let groups_list = group_list_data["group_list"];
+					// @todo better way to remove a item in the middle of a list
 					let new_groups_list = [];
-					for (let group_name of groups_list)
+					for (let group_item of groups_list)
 					{
-						if (group_name != name)
+						if (group_item != name)
 						{
-							new_groups_list.push(group_name);
-						}
-						else
-						{
-							new_groups_list.push(new_group_name);
+							new_groups_list.push(group_item);
 						}
 					}
 					browser.storage.local.set({group_list: new_groups_list}).then(() =>
 					{
-						console.log("CHANGED GROUP NAME");
+						browser.storage.local.remove(name);
+						browser.tabs.reload();
 					});
 				});
 			}
-			browser.tabs.reload();
+		});
+
+		function channel_title_from_id(channel_id)
+		{
+			let request = new XMLHttpRequest();
+			request.open("GET", FEED_BASE_URL + channel_id, false);
+			request.send(null);
+			let channel = (new DOMParser()).parseFromString(request.responseText, "text/xml");
+			let title = channel.getElementsByTagName("title")[0].innerHTML;
+			return title;
+		}
+
+		let title_input = group_config.querySelector("#title");
+		let apply_button = group_config.querySelector("#apply");
+		let cancel_button = group_config.querySelector("#cancel");
+		let channel_box = group_config.querySelector("#channel_box");
+
+		let group_data_init = {};
+		group_data_init[name] = [];
+		let config_channel_list = [];
+		browser.storage.local.get(group_data_init).then(group_data =>
+		{
+			config_channel_list = group_data[name];
+			function channel_box_update()
+			{
+				while (channel_box.firstChild) {channel_box.removeChild(channel_box.lastChild);}
+
+				let local_config_channel_list = config_channel_list;
+				for (let channel_id of local_config_channel_list)
+				{
+					let config_entry = config_entry_temp.querySelector("div").cloneNode(true);
+					config_entry.querySelector("div").innerText = channel_title_from_id(channel_id);
+					config_entry.querySelector("button").innerText = "X";
+					config_entry.querySelector("button").addEventListener("click", evt =>
+					{
+						// @todo better remove algorithm
+						let new_config_channel_list = []
+						for (let channel_item of config_channel_list)
+						{
+							if (channel_id != channel_item)
+							{
+								new_config_channel_list.push(channel_item);
+							}
+						}
+						config_channel_list = new_config_channel_list;
+						channel_box_update();
+					});
+					channel_box.appendChild(config_entry);
+				}
+			}
+			channel_box_update();
+		});
+
+		cancel_button.innerText = "Cancel";
+		cancel_button.addEventListener("click", evt => {browser.tabs.reload();});
+
+		title_input.value = name;
+		apply_button.innerText = "Apply Changes";
+		apply_button.addEventListener("click", evt =>
+		{
+			let config_group_data_tmp = {};
+			config_group_data_tmp[name] = config_channel_list;
+			browser.storage.local.set(config_group_data_tmp).then(() =>
+			{
+				let new_group_name = title_input.value;
+				if ((new_group_name != name) && (new_group_name != ""))
+				{
+					let group_data_tmp = {};
+					group_data_tmp[name] = [];
+					browser.storage.local.get(group_data_tmp).then(group_data =>
+					{
+						group_data_tmp = {};
+						group_data_tmp[new_group_name] = group_data[name];
+						browser.storage.local.set(group_data_tmp).then(() =>
+						{
+							browser.storage.local.remove(name);
+							browser.storage.local.get({group_list: []}).then(group_list_data =>
+							{
+								let groups_list = group_list_data["group_list"];
+								groups_list[groups_list.indexOf(name)] = new_group_name;
+								browser.storage.local.set({group_list: groups_list}).then(() =>
+								{
+									browser.tabs.reload();
+								});
+							});
+						});
+					});
+				}
+				else
+				{
+					browser.tabs.reload();
+				}
+			});
+			console.log("CHANGES APPLIED")
 		});
 		videos.appendChild(group_config);
 	}
